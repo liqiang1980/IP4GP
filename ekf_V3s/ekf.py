Rotation.as_rotvec -> wrong

ekf_V3s/ekf.py
@@ -110,13 +110,15 @@ class EKF:
                       tacperception.is_th_contact]
         for i in range(4):
             c_point_name = tacperception.get_contact_taxel_name(sim, model, hand_param[i + 1][0], z_h_flag="z")
-            # if c_point_name is None:
-            if not is_contact[i]:
+            if c_point_name is None:
+            # if not is_contact[i]:
                 print("none")
                 c_point_name = tacperception.last_contact[i][0]
-            print("_ju cname: ", c_point_name)
-            cur_s = tacperception.contact_renew(sim=sim, idx=i, tac_name=c_point_name, model="cur")
+            # print("_ju cname: ", c_point_name)
+            cur_s = tacperception.contact_renew(sim=sim, idx=i, tac_name=c_point_name, model="cur", xstate=x_state)
+            # cur_s = tacperception.contact_renew(sim=sim, idx=i, tac_name=tacperception.last_contact[i][0], model="cur", xstate=x_state)
             print(tacperception.cur_contact[i][1], tacperception.last_contact[i][1])
+            # print(is_contact)
             # _ju.extend(tacperception.cur_contact[i][1] - tacperception.last_contact[i][1])
             if is_contact[i]:
                 _ju.extend(tacperception.cur_contact[i][1] - tacperception.last_contact[i][1])
@@ -127,7 +129,7 @@ class EKF:
         ju_judge = ju - _ju
         print("..2 ju compare: ", ju_judge.shape, ju_judge)\
         # print("\n_ju: ", _ju)
-        ju = _ju
+        # ju = _ju
 
         prediction = np.matmul(G_pinv, ju)
         # pq_cup_in_palm = ug.get_relative_posquat(sim, src="palm_link", tgt="cup")

ekf_V3s/robot_control.py
@@ -1002,7 +1002,8 @@ class ROBCTRL:
             x_state[6] = pos_contact0[0][0]
             x_state[7] = pos_contact0[0][1]
             x_state[8] = pos_contact0[0][2]
-            tacperception.contact_renew(sim=sim, idx=0, tac_name=c_point_name0, model="last")
+            tacperception.contact_renew(sim=sim, idx=0, tac_name=c_point_name0, model="last", xstate=x_state)
+            # tacperception.contact_renew(sim=sim, idx=0, tac_name=tacperception.last_contact[0][0], model="last", xstate=x_state)
 
         if tacperception.is_finger_contact(sim, hand_param[2][0]) == True:
             c_point_name0 = tacperception.get_contact_taxel_name(sim, model, hand_param[2][0], z_h_flag="h")
@@ -1011,7 +1012,8 @@ class ROBCTRL:
             x_state[9] = pos_contact0[0][0]
             x_state[10] = pos_contact0[0][1]
             x_state[11] = pos_contact0[0][2]
-            tacperception.contact_renew(sim=sim, idx=1, tac_name=c_point_name0, model="last")
+            tacperception.contact_renew(sim=sim, idx=1, tac_name=c_point_name0, model="last", xstate=x_state)
+            # tacperception.contact_renew(sim=sim, idx=1, tac_name=tacperception.last_contact[1][0], model="last", xstate=x_state)
 
         if tacperception.is_finger_contact(sim, hand_param[3][0]) == True:
             c_point_name0 = tacperception.get_contact_taxel_name(sim, model, hand_param[3][0], z_h_flag="h")
@@ -1020,7 +1022,8 @@ class ROBCTRL:
             x_state[12] = pos_contact0[0][0]
             x_state[13] = pos_contact0[0][1]
             x_state[14] = pos_contact0[0][2]
-            tacperception.contact_renew(sim=sim, idx=2, tac_name=c_point_name0, model="last")
+            tacperception.contact_renew(sim=sim, idx=2, tac_name=c_point_name0, model="last", xstate=x_state)
+            # tacperception.contact_renew(sim=sim, idx=2, tac_name=tacperception.last_contact[2][0], model="last", xstate=x_state)
 
         if tacperception.is_finger_contact(sim, hand_param[4][0]) == True:
             c_point_name0 = tacperception.get_contact_taxel_name(sim, model, hand_param[4][0], z_h_flag="h")
@@ -1030,7 +1033,8 @@ class ROBCTRL:
             x_state[15] = pos_contact0[0][0]
             x_state[16] = pos_contact0[0][1]
             x_state[17] = pos_contact0[0][2]
-            tacperception.contact_renew(sim=sim, idx=3, tac_name=c_point_name0, model="last")
+            tacperception.contact_renew(sim=sim, idx=3, tac_name=c_point_name0, model="last", xstate=x_state)
+            # tacperception.contact_renew(sim=sim, idx=3, tac_name=tacperception.last_contact[3][0], model="last", xstate=x_state)
         return x_state
 
     def augmented_state(self, sim, model, hand_param, tacperception, x_state):
@@ -1039,40 +1043,44 @@ class ROBCTRL:
             pos_contact0 = ug.get_relative_posquat(sim, "cup", c_point_name0)[:3] + np.random.normal(0, 0.0,
                                                                                                      size=(1, 3))
             x_state = np.append(x_state, [pos_contact0])
-            tacperception.contact_renew(sim=sim, idx=0, tac_name=c_point_name0, model="last")
+            tacperception.contact_renew(sim=sim, idx=0, tac_name=c_point_name0, model="last", xstate=x_state)
+            # tacperception.contact_renew(sim=sim, idx=0, tac_name=tacperception.last_contact[0][0], model="last", xstate=x_state)
         else:
             x_state = np.append(x_state, [0, 0, 0])
-            tacperception.contact_renew(sim=sim, idx=0, tac_name=tacperception.last_contact[0][0], model="last")
+            tacperception.contact_renew(sim=sim, idx=0, tac_name=tacperception.last_contact[0][0], model="last", xstate=x_state)
 
         if tacperception.is_finger_contact(sim, hand_param[2][0]) == True:
             c_point_name0 = tacperception.get_contact_taxel_name(sim, model, hand_param[2][0], z_h_flag="h")
             pos_contact0 = ug.get_relative_posquat(sim, "cup", c_point_name0)[:3] + np.random.normal(0, 0.0,
                                                                                                      size=(1, 3))
             x_state = np.append(x_state, [pos_contact0])
-            tacperception.contact_renew(sim=sim, idx=1, tac_name=c_point_name0, model="last")
+            tacperception.contact_renew(sim=sim, idx=1, tac_name=c_point_name0, model="last", xstate=x_state)
+            # tacperception.contact_renew(sim=sim, idx=1, tac_name=tacperception.last_contact[1][0], model="last", xstate=x_state)
         else:
             x_state = np.append(x_state, [0, 0, 0])
-            tacperception.contact_renew(sim=sim, idx=1, tac_name=tacperception.last_contact[1][0], model="last")
+            tacperception.contact_renew(sim=sim, idx=1, tac_name=tacperception.last_contact[1][0], model="last", xstate=x_state)
 
         if tacperception.is_finger_contact(sim, hand_param[3][0]) == True:
             c_point_name0 = tacperception.get_contact_taxel_name(sim, model, hand_param[3][0], z_h_flag="h")
             pos_contact0 = ug.get_relative_posquat(sim, "cup", c_point_name0)[:3] + np.random.normal(0, 0.0,
                                                                                                      size=(1, 3))
             x_state = np.append(x_state, [pos_contact0])
-            tacperception.contact_renew(sim=sim, idx=2, tac_name=c_point_name0, model="last")
+            tacperception.contact_renew(sim=sim, idx=2, tac_name=c_point_name0, model="last", xstate=x_state)
+            # tacperception.contact_renew(sim=sim, idx=2, tac_name=tacperception.last_contact[2][0], model="last", xstate=x_state)
         else:
             x_state = np.append(x_state, [0, 0, 0])
-            tacperception.contact_renew(sim=sim, idx=2, tac_name=tacperception.last_contact[2][0], model="last")
+            tacperception.contact_renew(sim=sim, idx=2, tac_name=tacperception.last_contact[2][0], model="last", xstate=x_state)
 
         if tacperception.is_finger_contact(sim, hand_param[4][0]) == True:
             c_point_name0 = tacperception.get_contact_taxel_name(sim, model, hand_param[4][0], z_h_flag="h")
             pos_contact0 = ug.get_relative_posquat(sim, "cup", c_point_name0)[:3] + np.random.normal(0, 0.0,
                                                                                                      size=(1, 3))
             x_state = np.append(x_state, [pos_contact0])
-            tacperception.contact_renew(sim=sim, idx=3, tac_name=c_point_name0, model="last")
+            tacperception.contact_renew(sim=sim, idx=3, tac_name=c_point_name0, model="last", xstate=x_state)
+            # tacperception.contact_renew(sim=sim, idx=3, tac_name=tacperception.last_contact[3][0], model="last", xstate=x_state)
         else:
             x_state = np.append(x_state, [0, 0, 0])
-            tacperception.contact_renew(sim=sim, idx=3, tac_name=tacperception.last_contact[3][0], model="last")
+            tacperception.contact_renew(sim=sim, idx=3, tac_name=tacperception.last_contact[3][0], model="last", xstate=x_state)
         return x_state
 
     def interaction(self, sim, model, viewer, hand_param, object_param, alg_param, \

ekf_V3s/tactile_perception.py
@@ -58,38 +58,32 @@ class cls_tactile_perception:
         # self.cur_contact_z = [["touch_0_3_6", np.zeros(6)], ["touch_7_3_6", np.zeros(6)],
         #                       ["touch_11_3_6", np.zeros(6)], ["touch_15_3_6", np.zeros(6)]]
         self.last_contact = [["touch_0_3_6", np.zeros(6)], ["touch_7_3_6", np.zeros(6)],
-                             ["touch_11_3_6", np.zeros(6)], ["touch_15_3_6", np.zeros(6)]]
+                             ["touch_11_3_6", np.zeros(6)], ["touch_15_4_7", np.zeros(6)]]
         self.cur_contact = [["touch_0_3_6", np.zeros(6)], ["touch_7_3_6", np.zeros(6)],
-                            ["touch_11_3_6", np.zeros(6)], ["touch_15_3_6", np.zeros(6)]]
+                            ["touch_11_3_6", np.zeros(6)], ["touch_15_4_7", np.zeros(6)]]
 
-    def contact_renew(self, sim, idx, tac_name, model):
-        # pq_tac_in_palm = ug.get_relative_posquat(sim=sim, src="palm_link", tgt=tac_name)
-        pq_tac_in_cup = ug.get_relative_posquat(sim=sim, src="cup", tgt=tac_name)
-        norvec_tac_in_cup, s = og.surface_cup(pq_tac_in_cup[0], pq_tac_in_cup[1], pq_tac_in_cup[2])
-        R_tac_in_cup = ug.vec2rot(vec=norvec_tac_in_cup)
-        # rotvec = Rotation.from_quat(posquat[3:]).as_rotvec()
-        pq_cup_in_palm = ug.get_relative_posquat(sim=sim, src="palm_link", tgt="cup")
-        pos_cup_in_palm = pq_cup_in_palm[:3]
-        R_cup_in_palm = Rotation.from_quat(pq_cup_in_palm[3:]).as_matrix()
-        pos_tac_in_palm = pos_cup_in_palm + np.matmul(R_cup_in_palm, pq_tac_in_cup[:3].T).T
-        R_tac_in_palm = np.matmul(R_cup_in_palm, R_tac_in_cup)
-        rotvec_tac_in_palm = Rotation.from_matrix(R_tac_in_palm).as_rotvec()
-        # if model == ["last", "z"]:
-        #     self.last_contact_z[idx][0] = tac_name
-        #     self.last_contact_z[idx][1][:3] = posquat[:3]
-        #     self.last_contact_z[idx][1][3:] = rotvec
-        # elif model == ["cur", "z"]:
-        #     self.cur_contact_z[idx][0] = tac_name
-        #     self.cur_contact_z[idx][1][:3] = posquat[:3]
-        #     self.cur_contact_z[idx][1][3:] = rotvec
-        # elif model == ["last", "h"]:
-        #     self.last_contact_h[idx][0] = tac_name
-        #     self.last_contact_h[idx][1][:3] = posquat[:3]
-        #     self.last_contact_h[idx][1][3:] = rotvec
-        # elif model == ["cur", "h"]:
-        #     self.cur_contact_h[idx][0] = tac_name
-        #     self.cur_contact_h[idx][1][:3] = posquat[:3]
-        #     self.cur_contact_h[idx][1][3:] = rotvec
+    def contact_renew(self, sim, idx, tac_name, model, xstate):
+        pq_tac_in_palm = ug.get_relative_posquat(sim=sim, src="palm_link", tgt=tac_name)
+        pos_tac_in_palm = pq_tac_in_palm[:3]
+        # rotvec_tac_in_palm = abs(Rotation.from_quat(pq_tac_in_palm[3:]).as_rotvec())
+        rotvec_tac_in_palm = Rotation.from_quat(pq_tac_in_palm[3:]).as_rotvec()
+        #
+        # pos_cup_in_palm = np.mat(xstate[:3])
+        # R_cup_in_palm = Rotation.from_rotvec(xstate[3:6]).as_matrix()
+        # T_cup_in_palm = np.mat(np.eye(4))
+        # T_cup_in_palm[:3, :3] = R_cup_in_palm
+        # T_cup_in_palm[:3, 3] = pos_cup_in_palm.T
+        # T_palm_in_cup = np.linalg.inv(T_cup_in_palm)
+        #
+        # pos_tac_in_cup = np.ravel((T_palm_in_cup[:3, 3] + np.matmul(T_palm_in_cup[:3, :3], pos_tac_in_palm.T)).T)
+        # print(pos_tac_in_cup)
+        #
+        # norvec_tac_in_cup, s = og.surface_cup(pos_tac_in_cup[0], pos_tac_in_cup[1], pos_tac_in_cup[2])
+        # R_tac_in_cup = ug.vec2rot(vec=norvec_tac_in_cup)
+        #
+        # R_tac_in_palm = np.matmul(R_cup_in_palm, R_tac_in_cup)
+        # rotvec_tac_in_palm = Rotation.from_matrix(R_tac_in_palm).as_rotvec()
+
         if model == "last":
             self.last_contact[idx][0] = tac_name
             self.last_contact[idx][1][:3] = pos_tac_in_palm